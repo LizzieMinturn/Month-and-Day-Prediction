be0aeb47ebd2fdafa13934af3c99e51a1f2a7819
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-04-04 08:14:50 -0400

    wrote all my cases for the days

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -21,5 +21,29 @@ public class Main {
 
         int dayOfWeek = (q+(13*(m+1)/5)+K+(K/4)+(J/4)-2*J)%7;
 
+        switch(dayOfWeek){
+            case 0:
+                System.out.println("You were born on a saturday");
+                break;
+            case 1:
+                System.out.println("You were born on a sunday");
+                break;
+            case 2:
+                System.out.println("You were born on a monday");
+                break;
+            case 3:
+                System.out.println("You were born on a tuesday");
+                break;
+            case 4:
+                System.out.println("You were born on a wednesday");
+                break;
+            case 5:
+                System.out.println("You were born on a thursday");
+                break;
+            case 6:
+                System.out.println("You were born on a friday");
+                break;
+        }
+
     }
 }
