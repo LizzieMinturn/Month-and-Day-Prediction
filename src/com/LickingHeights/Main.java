9e9cbfd7b82d008291ea88e47ad8d6e55ddc59d7
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-04-04 07:50:00 -0400

    commented equation and declared/initialized scanner

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,13 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// h=(q+(13*(m+1)/5)+K+(K/4)+(J/4)-2J)
+
+        Scanner keyboard = new Scanner(System.in);
+      
+    }
+}
