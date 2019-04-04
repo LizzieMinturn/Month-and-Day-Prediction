09f020c331e6bc00b6812d71cb6d307dc907f6cf
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-04-04 07:51:45 -0400

    got input for the month the user was born in

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,6 +8,9 @@ public class Main {
 	// h=(q+(13*(m+1)/5)+K+(K/4)+(J/4)-2J)
 
         Scanner keyboard = new Scanner(System.in);
-      
+
+        System.out.println("What month were you born in?");
+        int m = keyboard.nextInt();//month
+
     }
 }
