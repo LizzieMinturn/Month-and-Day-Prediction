5380dfbae79c2963e438922529f1462cf62f4c90
lizzie <elizabeth.minturn.2021@student.lhschools.org>
2019-04-04 07:55:15 -0400

    got input for the year and declared/initialized the equation

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -13,6 +13,13 @@ public class Main {
         int m = keyboard.nextInt();//month
         System.out.println("What day of the month were you born on?");
         int q = keyboard.nextInt();//day
-       
+        System.out.println("What year were you born in?");
+        int year = keyboard.nextInt();
+
+        int J = year/100;
+        int K = year%100;
+
+        int dayOfWeek = (q+(13*(m+1)/5)+K+(K/4)+(J/4)-2*J)%7;
+
     }
 }
